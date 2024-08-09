66a94b8bce5902a4df3c100ed8fcebf47ea54de4
dmits <dmitsh7@gmail.com>
2024-08-09 16:16:38 +0400

    Triangle appears

---
+++ setup.bat
@@ -0,0 +1,3 @@
+mkdir "compiled shaders"
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shader.vert -o "compiled shaders/vert.spv"
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shader.frag -o "compiled shaders/frag.spv"

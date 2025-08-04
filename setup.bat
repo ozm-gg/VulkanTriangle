0e8b47f4779cac69281b1be0fb3388dcaecccc32
dmits <dmitsh7@gmail.com>
2025-08-04 21:21:26 +0300

    End of the vulkan tutorial

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 mkdir "compiled shaders"
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shader.vert -o "compiled shaders/vert.spv"
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe shader.frag -o "compiled shaders/frag.spv"
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shader.vert -o "compiled shaders/vert.spv"
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shader.frag -o "compiled shaders/frag.spv"
